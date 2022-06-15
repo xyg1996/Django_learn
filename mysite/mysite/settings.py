ae291a1935769988cb35edf4e0aad69ed0dc60c1
110% <xtmals@163.com>
2022-06-15 10:35:12 +0800

    add backgroud and modify admin

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -104,9 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
+#'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
+#'UTC'
 
 USE_I18N = True
 
